180ce5a36617c01f71c483011cfcca937faaaa6b
MostafaOS21 <mostafaos2021fp@gmail.com>
2023-04-14 15:36:35 +0200

    Some overflow changes

--- src/components/sub-components/GetUsers.jsx
+++ src/components/sub-components/GetUsers.jsx
@@ -10,7 +10,9 @@ const GetUsers = ({users}) => {
       <div className='user__avatar'>
         <img src={user.avatar_url} alt="User profile avatar" />
       </div>
-      <h4 className='user__name'>{user.login}</h4>
+      <h4 className='user__name'>{
+        user.login.length > 10 ? user.login.slice(0, 11) + ".." : user.login
+      }</h4>
       <a href={user.html_url} target="_blank"><AiOutlineLink /></a>
     </div>
   ))
